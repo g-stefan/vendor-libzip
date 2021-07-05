e0c136bf0dc4e11434a9cd33514ea0019782d2aa
Grigore Stefan <g_stefan@yahoo.com>
2021-07-05 14:10:57 +0300

    update

---
+++ build/msvc.install.sub.cmd
@@ -0,0 +1,27 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+rem --- make
+
+cmd.exe /C build\msvc.make.cmd
+if errorlevel 1 exit 1
+
+rem ---
+
+if not exist "%INSTALL_PATH_BIN%\" mkdir "%INSTALL_PATH_BIN%"
+xcopy /Y /S /E "temp\libzip\bin\*.exe" "%INSTALL_PATH_BIN%\"
+xcopy /Y /S /E "temp\libzip\bin\*.dll" "%INSTALL_PATH_BIN%\"
+
+rem --- dev
+
+if not exist "%INSTALL_PATH_DEV%\" mkdir "%INSTALL_PATH_DEV%"
+if not exist "%INSTALL_PATH_DEV%\bin" mkdir "%INSTALL_PATH_DEV%\bin"
+if not exist "%INSTALL_PATH_DEV%\include" mkdir "%INSTALL_PATH_DEV%\include"
+if not exist "%INSTALL_PATH_DEV%\lib" mkdir "%INSTALL_PATH_DEV%\lib"
+xcopy /Y /S /E "temp\libzip\bin\*.exe" "%INSTALL_PATH_DEV%\bin\"
+xcopy /Y /S /E "temp\libzip\bin\*.dll" "%INSTALL_PATH_DEV%\bin\"
+xcopy /Y /S /E "temp\libzip\include\" "%INSTALL_PATH_DEV%\include\"
+copy /Y /B "temp\libzip\lib\*.lib" "%INSTALL_PATH_DEV%\lib\*"
+
