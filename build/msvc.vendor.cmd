fb3803aabb3846899ac2758761626a1ef7452b23
Grigore Stefan <g_stefan@yahoo.com>
2021-11-15 23:10:42 +0200

    Update

--- build/msvc.vendor.cmd
+++ build/msvc.vendor.cmd
@@ -23,7 +23,7 @@ del /F /Q archive\%PROJECT%-%VERSION%.7z goto:eof
 rem Source
 pushd archive
 set VENDOR=%PROJECT%-%VERSION%
-set WEB_LINK=https://github.com/nih-at/libzip/releases/download/v1.7.3/libzip-1.7.3.tar.gz
+set WEB_LINK=https://github.com/nih-at/libzip/releases/download/v1.8.0/libzip-1.8.0.tar.gz
 if not exist %VENDOR%.tar.gz curl --insecure --location %WEB_LINK% --output %VENDOR%.tar.gz
 7z x %VENDOR%.tar.gz -so | 7z x -aoa -si -ttar -o.
 del /F /Q %VENDOR%.tar.gz
